faf560ed221c1b42bd0a51b2e9af30aaa07b3cad
Ralph Bean <rbean@redhat.com>
2012-02-17 10:00:04 -0500

    PEP8 formatting.

--- setup.py
+++ setup.py
@@ -5,20 +5,27 @@ import re
 from distutils.core import setup
 from src.pypi2spec import __version__
 
+description = "Small library to help you generate spec file for pypi project."
+
+long_desription = """
+pypi2spec makes you life easier at packaging pypi project for Fedora.
+"""
+
+download_url = "http://github.com/pypingou/pypi2spec-%s.tar.gz" % __version__
+
 setup(
-    name = 'pypi2spec',
-    version = __version__,
-    description = "Small library to help you generate spec file for pypi project.",
-    author = "Pierre-Yves Chibon",
-    author_email = "pingou@pingoured.fr",
-    maintainer = "Pierre-Yves Chibon",
-    maintainer_email = "pingou@pingoured.fr",
-    url = "http://github.com/pypingou/pypi2spec",
-    license = "GPLv3+",
-    long_description = 'pypi2spec makes you life easier at packaging pypi project for Fedora',
-    download_url = "http://github.com/pypingou/pypi2spec-%s.tar.gz" % __version__,
-    package_dir = {'pypi2spec': 'src/pypi2spec'},
-    packages = ['pypi2spec'],
+    name='pypi2spec',
+    version=__version__,
+    description=description,
+    author="Pierre-Yves Chibon",
+    author_email="pingou@pingoured.fr",
+    maintainer="Pierre-Yves Chibon",
+    maintainer_email="pingou@pingoured.fr",
+    url="http://github.com/pypingou/pypi2spec",
+    license="GPLv3+",
+    long_description=long_description,
+    download_url=download_url,
+    package_dir={'pypi2spec': 'src/pypi2spec'},
+    packages=['pypi2spec'],
     scripts=['pypi2spec'],
 )
-
